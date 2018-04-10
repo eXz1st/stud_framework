47a1551cbc63c8ee39cc0559f3968104011beab1
eXz1st <kiryan.maksim@gmail.com>
2018-04-10 10:37:55 +0300

    Model CRUD added

--- src/DB/DBOConnectorInterface.php
+++ src/DB/DBOConnectorInterface.php
@@ -7,12 +7,33 @@ namespace Mindk\Framework\DB;
 interface DBOConnectorInterface
 {
     /**
+     * Prepare sql query
+     *
+     * @param $sql
+     * @return mixed
+     */
+    public function prepareQuery($sql);
+    /**
+     * Execute data
+     *
+     * @parm $data
+     * @return mixed
+     */
+    public function executeData($data);
+    /**
      * Set sql query
      *
      * @param $sql
      * @return mixed
      */
     public function setQuery($sql);
+    /**
+     * Exec sql query
+     *
+     * @param $sql
+     * @return mixed
+     */
+    public function execQuery($sql);
     /**
      * Get single row result
      *
